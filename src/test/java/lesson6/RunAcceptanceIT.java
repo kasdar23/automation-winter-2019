67ccf3500de1687d4fd26cc7f354bc0ba28205c4
Dmitry_Lebedev <Dmitry_Lebedev1@epam.com>
2019-02-18 15:03:46 +0300

    Check access

--- src/test/java/lesson6/RunAcceptanceIT.java
+++ src/test/java/lesson6/RunAcceptanceIT.java
@@ -14,6 +14,7 @@ public class RunAcceptanceIT extends AbstractTestNGCucumberTests {
 
     @BeforeSuite()
     public void driverSetUp() {
+        // Checked
         Configuration.browser = Browsers.CHROME;
         Configuration.timeout = 5000;
         Configuration.pollingInterval = 300;
